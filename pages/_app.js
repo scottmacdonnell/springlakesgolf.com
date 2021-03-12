f0ddd124375996616f080a373214b4ff86fe2946
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-03-12 12:33:48 -0500

    Minor changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { AuthProvider } from '../lib/auth'
+
 import '../styles/globals.scss'
 
 function springlakesgolf({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default springlakesgolf
