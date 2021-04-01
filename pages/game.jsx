cc3a1b324484fe7b58ae2a0bb395914b360d300d
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-04-01 14:15:32 -0400

    Add GameShowcase component

--- pages/game.jsx
+++ pages/game.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import Paragraph from '../components/Paragraph'
 import ImageFeature from '../components/ImageFeature'
+import GameShowcase from '../components/GameShowcase'
 
 export default function Game() {
   return (
@@ -31,6 +32,8 @@ export default function Game() {
           <h2>Meet Shayne Dysart</h2>
           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
         </ImageFeature>
+
+        <GameShowcase />
       </main>
 
       <footer>
