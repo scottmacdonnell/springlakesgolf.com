be459470a06ddbd0ed55c26d00b0307149f10536
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-04-01 12:10:20 -0400

    Move Container inside MembershipForm component

--- components/MembershipForm.jsx
+++ components/MembershipForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import Container from './Container'
+
 import styles from '../styles/components/MembershipForm.module.scss'
 
 export default function MembershipForm() {
@@ -8,57 +10,59 @@ export default function MembershipForm() {
   const toggle = () => setPreviousCourseShown(!previousCourseShown)
 
   return (
-    <div className={styles.MembershipForm}>
-      <h1>Membership Form</h1>
+    <Container>
+      <div className={styles.MembershipForm}>
+        <h1>Membership Form</h1>
 
-      <form action="" method="post">
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" placeholder="John Doe" id="name" />
-        </div>
-
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" placeholder="johndoe@gmail.com" htmlFor="email" />
-        </div>
-
-        <div>
-          <label htmlFor="phone">Phone:</label>
-          <input type="tel" placeholder="(123)456-789" id="phone" />
-        </div>
-
-        <div className={styles.FormDate}>
-          <label htmlFor="date">Date of Birth:</label>
-          <input type="date" id="date" />
-        </div>
-
-        <div className={styles.FormCheckbox}>
-          <label htmlFor="previousMembership">Previously a Member:
-            <input type="checkbox" id="previousMembership" onChange={toggle} />
-          </label>
-        </div>
-
-        {previousCourseShown ? (
+        <form action="" method="post">
           <div>
-            <label htmlFor="previousCourse">previousCourse:</label>
-            <input type="text" id="previousCourse" />
+            <label htmlFor="name">Name:</label>
+            <input type="text" placeholder="John Doe" id="name" />
           </div>
-        ) : ''}
 
-        <div>
-          <label htmlFor="referral">Membership Referral:</label>
-          <input type="text" placeholder="Jane Doe" id="referral" />
-        </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" placeholder="johndoe@gmail.com" htmlFor="email" />
+          </div>
 
-        <div>
-          <label htmlFor="comments">Comments:</label>
-          <input type="text" id="comments" />
-        </div>
+          <div>
+            <label htmlFor="phone">Phone:</label>
+            <input type="tel" placeholder="(123)456-789" id="phone" />
+          </div>
 
-        <div>
-          <input type="submit" value="Submit"/>
-        </div>
-      </form>
-    </div>
+          <div className={styles.FormDate}>
+            <label htmlFor="date">Date of Birth:</label>
+            <input type="date" id="date" />
+          </div>
+
+          <div className={styles.FormCheckbox}>
+            <label htmlFor="previousMembership">Previously a Member:
+              <input type="checkbox" id="previousMembership" onChange={toggle} />
+            </label>
+          </div>
+
+          {previousCourseShown ? (
+            <div>
+              <label htmlFor="previousCourse">previousCourse:</label>
+              <input type="text" id="previousCourse" />
+            </div>
+          ) : ''}
+
+          <div>
+            <label htmlFor="referral">Membership Referral:</label>
+            <input type="text" placeholder="Jane Doe" id="referral" />
+          </div>
+
+          <div>
+            <label htmlFor="comments">Comments:</label>
+            <input type="text" id="comments" />
+          </div>
+
+          <div>
+            <input type="submit" value="Submit"/>
+          </div>
+        </form>
+      </div>
+    </Container>
   )
 }
--- pages/memberships.jsx
+++ pages/memberships.jsx
@@ -26,9 +26,7 @@ export default function Memberships() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ipsam ducimus beatae iste soluta placeat nesciunt atque impedit, nisi quo asperiores quibusdam qui sed suscipit, sint nulla quos dicta ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem sunt quidem blanditiis fuga eligendi dolore, nam esse similique optio? Maiores voluptatibus fugiat ut inventore officia ex iusto aliquam accusamus dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, harum aliquid excepturi saepe, nihil ab quasi explicabo voluptatibus quis ex culpa nisi placeat sed error cupiditate similique velit perferendis dolore!
         </Paragraph>
 
-        <Container>
-          <MembershipForm />
-        </Container>
+        <MembershipForm />
       </main>
 
       <footer>
