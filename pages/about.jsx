b461b949b47cb19c622f51a6214e3cf8f00184f1
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-04-02 20:09:52 -0400

    Add ImageFeature to about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -21,15 +21,14 @@ export default function About() {
           image="/images/about/banner.jpg"
         />
 
-        {/* <ImageFeature
+        <ImageFeature
           imageSrc="/images/about/imagefeature-message.jpg"
-          imageWidth={720}
-          imageHeight={1080}
+          imageRight
         >
           <h2>A word from the owner...</h2>
 
           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-        </ImageFeature> */}
+        </ImageFeature>
 
         <Team />
       </main>
