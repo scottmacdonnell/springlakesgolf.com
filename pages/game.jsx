7ed17a085cfb6f144957683298ebe96efb6ab197
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-04-16 20:16:55 -0400

    Rip Shayne

--- pages/game.jsx
+++ pages/game.jsx
@@ -1,8 +1,6 @@
 import Page from '../components/Page'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
-import TextFeature from '../components/ui/TextFeature'
-import ImageFeature from '../components/ImageFeature'
 import GameShowcase from '../components/GameShowcase'
 import Footer from '../components/Footer'
 
@@ -22,17 +20,6 @@ export default function Game() {
           title="Game Improvement"
           image="/images/game/banner.jpg"
         />
-
-        <TextFeature>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ipsam ducimus beatae iste soluta placeat nesciunt atque impedit, nisi quo asperiores quibusdam qui sed suscipit, sint nulla quos dicta ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem sunt quidem blanditiis fuga eligendi dolore, nam esse similique optio? Maiores voluptatibus fugiat ut inventore officia ex iusto aliquam accusamus dignissimos. </p>
-        </TextFeature>
-
-        <ImageFeature
-          imageSrc="/images/game/shayne.jpg"
-        >
-          <h2>Meet Shayne Dysart</h2>
-          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-        </ImageFeature>
 
         <GameShowcase />
       </main>
