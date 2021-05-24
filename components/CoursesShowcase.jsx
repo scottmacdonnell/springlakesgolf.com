be4a07e7b20b51dcefcaef99c46195af858e5016
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-05-24 18:22:52 -0400

    Add PinLocations to CourseShowcase component

--- components/CoursesShowcase.jsx
+++ components/CoursesShowcase.jsx
@@ -13,18 +13,21 @@ export default function CoursesShowcase(props) {
           title="The North Course"
           link="/courses/north"
           image="/images/courses/north/banner.jpg"
+          pinLocation="/docs/courses/north-location.pdf"
         />
         
         <CourseCard
           title="The South Course"
           link="/courses/south"
           image="/images/courses/south/banner.jpg"
+          pinLocation="/docs/courses/south-location.pdf"
         />
         
         <CourseCard
           title="The East Course"
           link="/courses/east"
           image="/images/courses/east/banner.jpg"
+          pinLocation="/docs/courses/east-location.pdf"
         />
       </div>
 
@@ -35,7 +38,8 @@ export default function CoursesShowcase(props) {
 function CourseCard({
   title,
   link,
-  image
+  image,
+  pinLocation
 }) {
   return (
     <div className={styles.CourseCard}>
@@ -46,7 +50,7 @@ function CourseCard({
               src={image}
               alt="Card Background"
               width={1080}
-              height={360}
+              height={540}
               layout="responsive"
               objectFit="cover"
             />
@@ -55,6 +59,20 @@ function CourseCard({
             <div className={styles.CourseCardContentWrapper}>
               <h2>{title}</h2>
             </div>
+          </div>
+          <div className={styles.PinLocation}>
+            
+            <Link href={pinLocation}>
+              <button className={styles.PinLocationButton}>
+                <span className={styles.Prefix}>
+                  <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="1.7" strokeLinecap="round" strokeLinejoin="round" fill="none" shapeRendering="geometricPrecision" >
+                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/>
+                    <circle cx="12" cy="10" r="3"/>
+                  </svg>
+                </span>
+                <span className={styles.Suffix}>Pin Locations</span>
+              </button> 
+            </Link>
           </div>
         </a>
       </Link>
