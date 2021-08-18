aaa30e5c18e630d59bcd4e9794fdeb6b8f463f4b
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-08-18 13:19:00 -0400

    Add Google Analytics (gtag.js)

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { AnimatePresence } from 'framer-motion'
 
 import { AuthProvider } from '../lib/auth'
+import * as gtag from '../lib/gtag'
 
 import '../styles/globals.scss'
 
 export default function SpringLakesGolfClub({ Component, pageProps }) {
   const router = useRouter()
+  
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class MainDocument extends Document {
+import { getLangFromReq } from '../utils/fromReq'
+import { MEASUREMENT_ID } from '../lib/gtag'
+
+export default class MainDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const lang = getLangFromReq(ctx.req)
+    return { ...initialProps, lang }
+  }
+  
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta name="og:type" property="og:type" content="website" key="og:type" />
-          <meta property="og:locale" content="en_US" key="og:locale" />
-          <meta name="og:site_name" property="og:site_name" content="Spring Lakes Golf Club" key="og:sitename" />
-          
-          <meta name="twitter:card" content="summary_large_image" key="twcard" />
-
+          {/* Favicon */}
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
           <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#845ef7" />
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
           <meta name="msapplication-TileColor" content="#2b5797" />
@@ -23,8 +26,36 @@ class MainDocument extends Document {
           <meta name="theme-color" content="#2f4671" media="(prefers-color-scheme: dark)" />
           <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
           <link rel="alternate" type="application/rss+xml" href="/favicon/feed.xml" />
-        </Head>
 
+          {/* Web Manifest */}
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+
+          {/* Global Meta Tags (Page.js) - OpenGraph */}
+          <meta name="og:type" property="og:type" content="website" key="og:type" />
+          <meta property="og:locale" content="en_US" key="og:locale" />
+          <meta name="og:site_name" property="og:site_name" content="Spring Lakes Golf Club" key="og:sitename" />
+
+          {/* Global Meta Tags (Page.js) - Twitter */}
+          <meta name="twitter:card" content="summary_large_image" key="twcard" />
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -33,5 +64,3 @@ class MainDocument extends Document {
     )
   }
 }
-
-export default MainDocument
